fb0d7f75cccc9bdbe911d16b642161f994899c42
vohrr <matthew.d.pingel@gmail.com>
2024-04-13 22:47:40 -0400

    implemented word count, created letter dictionary, created report

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 
-def main():
-    with open("books/frankenstein.txt") as f:
+def main(filepath:str):
+    with open(filepath) as f:
         file_contents = f.read()
-        print(get_word_count(file_contents))
+        word_count = get_word_count(file_contents)
+        letter_dictionary = create_dictionary(file_contents)
+        print_report(word_count, letter_dictionary, filepath)
 
 def get_word_count(contents):
     return len(contents.split())
 
-main()
+def create_dictionary(contents):
+    lower_string = contents.lower()
+    letter_dictionary = dict()
+    for index in lower_string:
+        if index in letter_dictionary:
+            letter_dictionary[index] += 1
+        else: 
+            letter_dictionary[index] = 1
+    return letter_dictionary
+
+def sort_on(letter_dictionary:dict):
+    return letter_dictionary[1]
+
+def print_report(word_count:int, letter_dictionary:dict, filepath:str):
+    letter_list:list = []
+    for letter in letter_dictionary:
+        if str(letter).isalpha():
+            letter_list.append([letter, letter_dictionary[letter]])
+    letter_list.sort(reverse=True,key = lambda x: x[1])
+    
+    print(f"--- Begin report of {filepath} ---")
+    print(f"{word_count} words found in the document")
+    for letter in letter_list:
+        print(f"The {letter[0]} character was found {letter[1]} times")
+    print("--- End Report ---")
+
+
+
+main("books/frankenstein.txt")
